0104701c5c7c9e1cd5a44ba19e1aac7f8538602b
Dorgival Rocha <dojak220@ufrn.edu.br>
2022-07-09 12:17:20 -0300

    Change project name

--- lib/src/utils/routes.dart
+++ lib/src/utils/routes.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:loomi_flutter_boilerplate/src/presentation/views/example_screen.dart';
+import 'package:sotintas/src/presentation/views/example_screen.dart';
 
 final Map<String, WidgetBuilder> routes = {
   ExampleScreen.routeName: (_) => const ExampleScreen(),
