47b2ebf50a01d0820352e7ec088e07fe87914a92
Dorgival Rocha <dojak220@ufrn.edu.br>
2022-07-14 20:09:23 -0300

    Refactoring store page to use controller and store

--- lib/src/presentation/views/store_screen.dart
+++ lib/src/presentation/views/store_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:get_it/get_it.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:sotintas/src/external/models/product.dart';
 import 'package:sotintas/src/presentation/controllers/store_controller.dart';
 
-import 'package:sotintas/src/presentation/stores/product_store.dart';
 import 'package:sotintas/src/presentation/views/ink_detail_page.dart';
 import 'package:sotintas/src/presentation/widgets/custom_appbar.dart';
+import 'package:sotintas/src/utils/helpers/display_size_helper.dart';
 
 class StoreScreen extends StatefulWidget {
   final StoreController controller;
@@ -17,7 +18,11 @@ class StoreScreen extends StatefulWidget {
 }
 
 class _StoreScreenState extends State<StoreScreen> {
-  final exampleStore = GetIt.I.get<ProductStore>();
+  @override
+  void initState() {
+    widget.controller.getProducts();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,18 +31,19 @@ class _StoreScreenState extends State<StoreScreen> {
       body: SafeArea(
         child: Column(
           children: [
-            const SearchField(),
-            const ListFilter(),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.73,
-              child: ListView.separated(
-                itemCount: 10,
-                itemBuilder: ((_, i) {
-                  return const InkList();
-                }),
-                separatorBuilder: (context, index) =>
-                    const SizedBox(height: 10),
-              ),
+            SearchField(controller: widget.controller),
+            ListFilter(controller: widget.controller),
+            Observer(
+              builder: (context) {
+                return SizedBox(
+                  height: displayUsableHeight(context) * 0.82,
+                  child: (widget.controller.isFetchingProducts)
+                      ? const Center(child: CircularProgressIndicator())
+                      : widget.controller.productStore.productCount == 0
+                          ? const Center(child: Text("Sem produtos à venda"))
+                          : ProductsListWidget(controller: widget.controller),
+                );
+              },
             ),
           ],
         ),
@@ -46,10 +52,31 @@ class _StoreScreenState extends State<StoreScreen> {
   }
 }
 
+class ProductsListWidget extends StatelessWidget {
+  const ProductsListWidget({
+    Key? key,
+    required this.controller,
+  }) : super(key: key);
 
+  final StoreController controller;
 
-class InkList extends StatelessWidget {
-  const InkList({
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      shrinkWrap: true,
+      itemCount: controller.productCount,
+      itemBuilder: ((_, i) {
+        return ProductCard(controller.products![i]!);
+      }),
+      separatorBuilder: (context, index) => const SizedBox(height: 10),
+    );
+  }
+}
+
+class ProductCard extends StatelessWidget {
+  final Product product;
+  const ProductCard(
+    this.product, {
     Key? key,
   }) : super(key: key);
 
@@ -57,13 +84,21 @@ class InkList extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       leading: Image.network(
-        "https://picsum.photos/200",
+        product.coverImage,
+        height: MediaQuery.of(context).size.height * 0.08,
+        loadingBuilder: (_, child, loadingProgress) {
+          return loadingProgress == null
+              ? child
+              : const CircularProgressIndicator();
+        },
       ),
-      subtitle: const Text("R\$ 100,00"),
-      title: const Text("Tinta"),
+      subtitle: Text(product.price.toString()),
+      title: Text(product.name),
       trailing: TextButton(
         onPressed: () {},
-        child: const Text("Entrega grátis"),
+        child: product.deliveryFree
+            ? const Text("Entrega grátis")
+            : const SizedBox(),
       ),
       onTap: () => Navigator.of(context).push(
         MaterialPageRoute<void>(
@@ -75,53 +110,64 @@ class InkList extends StatelessWidget {
 }
 
 class SearchField extends StatelessWidget {
-  const SearchField({
-    Key? key,
-  }) : super(key: key);
+  const SearchField({Key? key, required this.controller}) : super(key: key);
+
+  final StoreController controller;
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      decoration: const InputDecoration(
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(50.0)),
+    return SizedBox(
+      height: displayUsableHeight(context) * 0.08,
+      child: TextFormField(
+        decoration: const InputDecoration(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(50.0)),
+          ),
+          prefixIcon: Icon(Icons.search_rounded),
+          hintText: "Buscar...",
         ),
-        prefixIcon: Icon(Icons.search_rounded),
-        hintText: "Buscar...",
+        controller: controller.searchController,
+        onEditingComplete: () {
+          controller.freeShippingFilter
+              ? controller.filterByDeliveryFree()
+              : controller.getProducts();
+        },
       ),
     );
   }
 }
 
-class ListFilter extends StatefulWidget {
-  const ListFilter({Key? key}) : super(key: key);
+class ListFilter extends StatelessWidget {
+  const ListFilter({Key? key, required this.controller}) : super(key: key);
 
-  @override
-  State<ListFilter> createState() => _ListFilterState();
-}
-
-class _ListFilterState extends State<ListFilter> {
-  bool _freeShippingFilter = false;
+  final StoreController controller;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.max,
-      children: [
-        Switch(
-          value: _freeShippingFilter,
-          onChanged: (newValue) {
-            setState(() => _freeShippingFilter = newValue);
-          },
-        ),
-        const Text("Apenas "),
-        const Text(
-          "entrega grátis",
-          style: TextStyle(decoration: TextDecoration.underline),
-        ),
-        const Spacer(),
-        const Text("100 resultados"),
-      ],
+    return SizedBox(
+      height: displayUsableHeight(context) * 0.06,
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Observer(
+            builder: ((_) => Switch(
+                  value: controller.freeShippingFilter,
+                  onChanged: (newValue) {
+                    newValue
+                        ? controller.filterByDeliveryFree()
+                        : controller.getProducts();
+                  },
+                )),
+          ),
+          const Text("Apenas "),
+          const Text(
+            "entrega grátis",
+            style: TextStyle(decoration: TextDecoration.underline),
+          ),
+          const Spacer(),
+          const Text("100 resultados"),
+        ],
+      ),
     );
   }
 }
---
+++ lib/src/utils/helpers/display_size_helper.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+double displayHeight(BuildContext context) {
+  return MediaQuery.of(context).size.height;
+}
+
+double displayWidth(BuildContext context) {
+  return MediaQuery.of(context).size.width;
+}
+
+double displayUsableHeight(BuildContext context) {
+  return MediaQuery.of(context).size.height -
+      kToolbarHeight -
+      MediaQuery.of(context).padding.top -
+      kBottomNavigationBarHeight;
+}
